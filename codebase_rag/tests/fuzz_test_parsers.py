2b9dea21ca56653cced5f7f1da10de21de1c144b
vitali87 <eheva87@gmail.com>
2026-03-02 00:21:32 +0000

    perf: move imports out of fuzz loop to top level

--- codebase_rag/tests/fuzz_test_parsers.py
+++ codebase_rag/tests/fuzz_test_parsers.py
@@ -2,15 +2,15 @@ import sys
 
 import atheris
 
+from codebase_rag.language_spec import (
+    get_language_for_extension,
+    get_language_spec,
+)
+
 
 def fuzz_language_spec(data):
     fdp = atheris.FuzzedDataProvider(data)
     extension = fdp.ConsumeUnicodeNoSurrogates(64)
-    from codebase_rag.language_spec import (
-        get_language_for_extension,
-        get_language_spec,
-    )
-
     get_language_spec(extension)
     get_language_for_extension(extension)
 
